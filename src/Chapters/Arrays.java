6fba5163cc0eaba336b08a3df1ee49e29f69e373
Christian T Flink <chri62aa@edu.easj.dk>
2018-10-04 13:25:23 +0200

    til og med opg. 1.11

---
+++ src/Chapters/Arrays.java
@@ -0,0 +1,22 @@
+package Chapters;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        //Her er lidt spas med arrays
+
+        int marks[] = {1,2,3,4,5};
+
+        //Her starter counteren på 0, printer derefter tallet fra arrayen ud, svarende til array pladsen som counteren er nået til
+        // derefter gør den counteren 1 større
+        for(int counter = 0; counter < marks.length ; counter++){
+
+            System.out.println(marks[counter]);
+        }
+        String navne[] = {"Christian","Dan",};
+        System.out.println(navne[1] + navne[0]);
+
+    }
+
+}
+
